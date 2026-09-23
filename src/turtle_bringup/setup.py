792e82d71f4857d3bedbbe6ef794cbd3a1b38509
Fantom-ctrl <mara.sirazieva@yandex.ru>
2026-09-23 14:44:40 +0700

    PR02 realization

---
+++ src/turtle_bringup/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+package_name = 'turtle_bringup'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Мария',
+    maintainer_email='m.slesareva@g.nsu.ru',
+    description='Пакет для работы с черепашкой',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
